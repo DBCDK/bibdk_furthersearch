9e732ac0ce2c50e9189f510ff1945452014060b0
Martin Møller <mmj@dbc.dk>
2013-03-27 14:25:48 +0100

    js added

--- js/bibdk_furthersearch.js
+++ js/bibdk_furthersearch.js
@@ -1,8 +1,28 @@
 (function ($) {
-    Drupal.behaviors.cart = {
+    Drupal.doSearch = function(element){
+        var tableClass = '.further-search-table-' + element.attr('data-id');
+        var table = $(tableClass);
+        var url = '((';
+        $(tableClass + ' input[type=checkbox]').each(function(){
+            if(this.checked){
+                //TODO sorting based on type, element, fiction - clarify sorting methods with MAR
+                console.log('checked');
+                console.log($(this).attr('data-code'));
+                url +=  $(this).attr('data-code');
+                url += '&'
+            } else {
+                console.log('not checked');
+            }
+        });
+        url += '))';
+        console.log('end');
+        console.log(url);
+    };
+
+    Drupal.behaviors.furthersearch = {
         attach:function (context) {
-            $('.hest-knap', context).click(function(){
-                console.log('hest');
+            $('.further-search-btn', context).click(function(){
+                Drupal.doSearch($(this));
             });
         }
     };

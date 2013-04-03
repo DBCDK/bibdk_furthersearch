e2238f8185076fe8298538f727643088500fefc6
Martin Møller <mmj@dbc.dk>
2013-04-03 14:41:39 +0200

    js solution implemented

--- js/bibdk_furthersearch.js
+++ js/bibdk_furthersearch.js
@@ -1,28 +1,93 @@
-(function ($) {
-    Drupal.doSearch = function(element){
+(function($) {
+    Drupal.doSearch = function(element) {
         var tableClass = '.further-search-table-' + element.attr('data-id');
-        var table = $(tableClass);
-        var url = '((';
-        $(tableClass + ' input[type=checkbox]').each(function(){
-            if(this.checked){
-                //TODO sorting based on type, element, fiction - clarify sorting methods with MAR
-                console.log('checked');
-                console.log($(this).attr('data-code'));
-                url +=  $(this).attr('data-code');
-                url += '&'
-            } else {
-                console.log('not checked');
+
+        var types = '';
+        var elements = '';
+        var fictions = '';
+
+        $(tableClass + ' input[type=checkbox]').each(function() {
+            if(this.checked) {
+                switch($(this).attr('data-type')) {
+                    case 'type':
+                        types = Drupal.addType(types, $(this).attr('data-code'));
+                        break;
+                    case 'element':
+                        elements = Drupal.addElement(elements, $(this).attr('data-code'));
+                        break;
+                    case 'fiction':
+                        fictions = Drupal.addFiction(fictions, $(this).attr('data-code'));
+                        break;
+                    default:
+                        throw new Error('no data-type provided');
+                        break;
+                }
             }
         });
-        url += '))';
-        console.log('end');
-        console.log(url);
+
+        var url = Drupal.makeUrl(types, elements, fictions);
+        window.open(Drupal.settings.basePath + Drupal.settings.pathPrefix + 'search/work/' + url, '_self');
+    };
+
+    Drupal.makeUrl = function(types, elements, fictions) {
+        var url = elements;
+        if(fictions !== ''){
+            url += ' AND ' + fictions;
+        }
+
+        if(types !== '') {
+            url += types;
+        }
+
+        return url;
+    };
+
+    Drupal.addType = function(types, type) {
+        if(types === '') {
+            types = '?facets[]=' + type;
+        } else {
+            types += '&' + type;
+        }
+        return types;
+    };
+
+    Drupal.addElement = function(elements, element) {
+        if(elements === '') {
+            elements = element;
+        } else {
+            elements += ' AND ' + element;
+        }
+        return elements;
+    };
+
+    Drupal.addFiction = function(fictions, type) {
+        if(fictions === '') {
+            fictions = type;
+        } else {
+            fictions += ' AND ' + type;
+        }
+        return fictions;
+    };
+
+    Drupal.isOneElementSelected = function(element) {
+        var tableClass = '.further-search-table-' + element.attr('data-id');
+        var counter = 0;
+        $(tableClass + ' input[type=checkbox]').each(function() {
+            if(this.checked && $(this).attr('data-type') === 'element') {
+                ++counter;
+            }
+        });
+        return counter;
     };
 
     Drupal.behaviors.furthersearch = {
-        attach:function (context) {
-            $('.further-search-btn', context).click(function(){
-                Drupal.doSearch($(this));
+        attach: function(context) {
+            $('.further-search-btn', context).click(function() {
+                if(Drupal.isOneElementSelected($(this))) {
+                    Drupal.doSearch($(this));
+                } else {
+                    alert(Drupal.t('alert_please_select_at_least_one_element', {}, {context: 'bibdk_furthersearch'}));
+                }
             });
         }
     };
